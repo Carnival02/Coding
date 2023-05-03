c8d64edf6d9e767c8697531e0c36ee612b00c94a
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-05-03 10:02:10 -0700

    Create question4.cpp

---
+++ pattern/question4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=n;
+    for(i;i>=1;i--)
+    { for(int j=1;j<=i;j++)
+    {
+        cout<<i;
+                
+       
+    }cout<<endl;
+    
+    
+
+    }
+return 0;
+} 
+/*
+
+55555
+4444
+333
+22
+1
+
+*/
